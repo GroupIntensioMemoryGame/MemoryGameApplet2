78797300c9bb9cfd69e5069620dc54dad4f3f7d5
ricpoirson <mlgric15z@gmail.com>
2013-04-28 18:38:16 -0500

    commit

--- src/view/PlayPanel.java
+++ src/view/PlayPanel.java
@@ -6,19 +6,30 @@ import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
+import model.ISimonSaysObserver;
 import model.Player;
+import model.SimonSays;
 import controller.SimonSaysController;
 
-public class PlayPanel extends JFrame implements ActionListener
+public class PlayPanel extends JFrame implements ActionListener, ISimonSaysObserver
 {
-    public PlayPanel(Player player, int numObjects, String layout, int size, ArrayList<String> colors, ArrayList<String> shapes){
+    public PlayPanel(Player player, int numObjects, String layout, int size, ArrayList<String> colors, ArrayList<String> shapes, final SimonSaysController sscgame){
         super("Simon Says");
         
+        sscgame.play(player, shapes, colors, numObjects, this);
+
         
     }
 
     @Override
     public void actionPerformed(ActionEvent e)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(SimonSays ss)
     {
         // TODO Auto-generated method stub
         
